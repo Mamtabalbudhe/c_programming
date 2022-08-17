fdb3b96add650aaf7ce2977ce9a571d1559b21df
Mamtabalbudhe <53394570+Mamtabalbudhe@users.noreply.github.com>
2022-08-17 00:23:03 -0700

    Add files via upload

---
+++ prg.c
@@ -0,0 +1,13 @@
+
+// Demonstrating Bit-Wise Oerator 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=5;
+	//clrscr();
+	printf("a :%d\n",a);
+	printf("a<<3:%d\n",a<<3);
+	a=a<<3;
+	printf("a>>2 :%d\n",a);
+}
